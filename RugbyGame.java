85e78b6410bd757ecdccf382b8ba7ee594cc274f
yuriy94 <uriyguba1994@gmail.com>
2023-07-06 23:45:28 +0300

    Step3

--- RugbyGame.java
+++ RugbyGame.java
@@ -1,34 +1,46 @@
 package game;
 
+import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class RugbyGame {
     public static void main(String[] args) {
-        System.out.print("Team Blue: ");
-        teamBlue();
-        System.out.println();
-        System.out.print("Team Red: ");
-        teamRed();
+
+
+        System.out.println("Середній вік гравців Червоної команди: ");
+        calculateAverageRed();
+        System.out.println("Середній вік гравців Блакитної команди: ");
+        calculateAverageBlue();
     }
 
-    public static void teamBlue() {
+    public static void calculateAverageRed() {
+        int averageRed = 0;
         int[] array = new int[25];
+        int sum = 0;
         for (int i = 0; i < array.length; i++) {
             array[i] = ThreadLocalRandom.current().nextInt(18, 40);
-            System.out.print(" " + "age:" + array[i] + ";");
+            sum += array[i];
         }
+        averageRed = sum / array.length;
+        System.out.println(averageRed);
     }
 
-    public static void teamRed() {
+    public static void calculateAverageBlue() {
+        int averageBlue = 0;
         int[] array = new int[25];
+        int sum = 0;
         for (int i = 0; i < array.length; i++) {
             array[i] = ThreadLocalRandom.current().nextInt(18, 40);
-            System.out.print(" " + "age:" + array[i] + ";");
+            sum += array[i];
         }
+        averageBlue = sum / array.length;
+        System.out.println(averageBlue);
     }
-    public static void calculateMediumAge(){
+
+    public static void calculateMediumAge() {
 
     }
+
 }
 
 
