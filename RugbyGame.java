a4d42a6026c0889f50b153788aa21e4dce7cb53f
yuriy94 <uriyguba1994@gmail.com>
2023-07-06 20:43:50 +0300

    Step2

--- RugbyGame.java
+++ RugbyGame.java
@@ -4,28 +4,40 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class RugbyGame {
     public static void main(String[] args) {
-
+        System.out.print("Team Blue: ");
+        teamBlue();
+        System.out.println();
+        System.out.print("Team Red: ");
+        teamRed();
     }
 
-    public static void teamBlue(int[] age1) {
-        for(int i = 0 ; i<age1.length; i++)
-        age1[i]=ThreadLocalRandom.current().nextInt(18, 40);
+    public static void teamBlue() {
+        int[] array = new int[25];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt(18, 40);
+            System.out.print(" " + "age:" + array[i] + ";");
+        }
     }
 
-    public static void teamRed(int[] age2) {
-        for(int a = 0 ; a<age2.length; a++)
-        age2[a]=ThreadLocalRandom.current().nextInt(18, 40);
+    public static void teamRed() {
+        int[] array = new int[25];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = ThreadLocalRandom.current().nextInt(18, 40);
+            System.out.print(" " + "age:" + array[i] + ";");
+        }
+    }
+    public static void calculateMediumAge(){
 
     }
-
-
-    //Є дві команди регбі з 25 гравців різного віку у кожній.
-    //
-    //Вік беремо випадковим чином в діапазоні від 18 до 40.
-    //
-    //Виведіть у двох рядках вік гравців кожної команди.
-    //
-    //Порахуйте середній вік гравців кожної команди та виведіть на екран.
-
-
 }
+
+
+//Є дві команди регбі з 25 гравців різного віку у кожній.
+//
+//Вік беремо випадковим чином в діапазоні від 18 до 40.
+//
+//Виведіть у двох рядках вік гравців кожної команди.
+//
+//Порахуйте середній вік гравців кожної команди та виведіть на екран.
+
+
